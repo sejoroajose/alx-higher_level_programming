89883fe7de8d5acedd459b2a5476f7e7e9043ce9
sejoroajose <ajosesejoro@gmail.com>
2023-11-02 00:30:32 +0100

    corrected

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
+
+if number < 0:
+    last = ((number * -1) % 10) * -1
+else:
+    last = number % 10
 
 msg = "and is "
 
