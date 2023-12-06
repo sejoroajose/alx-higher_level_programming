bffc1e99ff5d48b5921760813b5972aba90d13aa
sejoroajose <ajosesejoro@gmail.com>
2023-12-06 03:34:50 +0100

    Handling Json string

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 101-stats.py
+# Ajose Sejoro <ajosesejoro2@gmail.com>
 """Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
