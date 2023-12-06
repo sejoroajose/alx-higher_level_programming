149fbadf1da7d1d1831ae9f2f6d5cd546c6ec8b0
sejoroajose <ajosesejoro@gmail.com>
2023-12-06 02:57:29 +0100

    Appending text to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+""" This module appends a string to  a text file """
+
+
+def append_write(filename="", text=""):
+
+    """
+        This function is used to append a string to a text file
+
+        Args:
+            filename (str): name of destination file
+            text (str): string to append
+
+        Returns:
+             The number of string appended
+    """
+
+    if filename:
+        try:
+            with open(filename, 'a', encoding='utf-8') as file:
+                chars_added = file.write(text)
+                return chars_added
+        except:
+            return 0
