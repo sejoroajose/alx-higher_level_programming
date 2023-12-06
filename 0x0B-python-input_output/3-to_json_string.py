d13dfde2995729c7c06a8b423978bc698d37b009
sejoroajose <ajosesejoro@gmail.com>
2023-12-06 03:14:42 +0100

    Handling Json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 import json
 
+""" This module reutrns the JSON representation of an object """
+
+
 def to_json_string(my_obj):
+    """
+        This function handles JSON files
+
+        Args:
+            my_obj (obj): object variable
+
+        Returns:
+            The JSON representation of an object string
+    """
+
     return json.dumps(my_obj)
