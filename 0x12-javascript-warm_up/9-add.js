8adee93467aea817e9aa77c5ac14d42bbd88b789
sejoroajose <ajosesejoro@gmail.com>
2024-07-06 13:38:54 +0100

    script to prints the addition of two arguments

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const [firstArg, secondArg] = process.argv.slice(2);
+
+function add(a, b) {
+  const num1 = parseInt(a);
+  const num2 = parseInt(b);
+
+  if(isNaN(num1) || isNaN(num2)) {
+    console.log('NaN');
+  } else {
+    console.log(num1 + num2);
+  }
+}
+
+add(firstArg, secondArg);
