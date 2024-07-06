e3778c6baa6eff13732151999ffcf1da9c05832d
sejoroajose <ajosesejoro@gmail.com>
2024-07-06 13:24:43 +0100

    script to print a square of x in the number of cmd argument

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+const x = parseInt(arg);
+
+if (isNaN(x) || x <= 0 ) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      process.stdout.write('X');
+    };
+    process.stdout.write('\n');
+  }
+}
