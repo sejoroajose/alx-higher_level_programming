7c92289b7d7d664a486a8ae1f4e6dbdc68627283
sejoroajose <ajosesejoro@gmail.com>
2024-07-06 14:07:12 +0100

    script to prints the factorial of an argument

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+
+const x = parseInt(arg, 10);
+
+function factorial (n) {
+  if (n <=1 ) return 1;
+  return n * factorial (n - 1);
+}
+
+const result = isNaN(x) ? 1 : factorial(x);
+
+console.log(result);
