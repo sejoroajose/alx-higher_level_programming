0ea9ef46a506cef57af21ae6ce43cd43732389a2
sejoroajose <ajosesejoro@gmail.com>
2024-07-09 13:04:01 +0100

    Script to create a rectangle with a width and height

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const square = require('./5-square.js');
+
+const Square = class extends square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+	}
+        process.stdout.write('\n');
+      }
+    }
+  }
+};
+
+module.exports = Square;
